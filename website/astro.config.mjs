4aec39d3662d30a5ed53e3707078ea74b3df9902
Adam Falk <adammfalk@gmail.com>
2024-06-23 14:46:46 -0500

    pages pub

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -4,6 +4,7 @@ import starlight from '@astrojs/starlight';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://adammfalk.github.io',
+  base: 'sirrs',
   integrations: [
     starlight({
       title: 'Society of Iowa Rugby Refs',
