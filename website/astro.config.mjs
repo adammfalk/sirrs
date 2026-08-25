2316dc82f812dbd0fa3906613a68f2ed5cf64ec1
Adam Falk <adammfalk@gmail.com>
2026-08-24 21:03:34 -0500

    removed unneeded page

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -48,10 +48,6 @@ export default defineConfig({
           label: "Bulletins",
           items: [
             {
-              label: "January 2025 Law Trials",
-              link: "/bulletins/january-2025-law-trials",
-            },
-            {
               label: "Law 3 and Replacements",
               link: "/bulletins/law-3-and-replacements",
             },
